6729fb04542a597894433f6fb78dd099026f24f6
JONATHAN ALBERTO RODRIGUEZ MARTINEZ <jony_elapcifico@hotmail.com>
2022-05-14 07:00:11 -0600

    conxion sql

---
+++ JonathanRo_MVc/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ADS_Project.Models;
+using JonathanRo_MVc.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace JonathanRo_MVc.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<EstudianteViewModel> Estudiantes { get; set; }
+
+        public DbSet<MateriaViewModel> Materias { get; set; }
+
+        public DbSet<ProfesorViewModel> Profesores { get; set; }
+
+        public DbSet<CarreraViewModel> Carreras { get; set; }
+
+        public DbSet<GruposViewModel> Grupos { get; set; }
+    }
+}
